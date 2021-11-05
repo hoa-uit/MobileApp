cc043eb20b62d8e3ff388fc1cee61369d80a66d2
ZachHung <zachhung911@gmail.com>
2021-11-05 22:52:23 +0700

    Basic Todo App

---
+++ components/Task.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#88C0D0",
+    marginRight: 15,
+    borderRadius: 5,
+  },
+  circular: {
+    width: 18,
+    height: 18,
+    borderColor: "#88C0D0",
+    borderWidth: 4,
+    borderRadius: 50,
+  },
+});
+export default Task;
